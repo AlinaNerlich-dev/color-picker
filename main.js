cb91132a9b8a209a667c34621e79ce5e21ab67a5
Alina <info@alina-nerlich.com>
2023-11-22 22:29:05 +0100

    add new Color_Palette

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import "./style.css";
 
 const Color_Palette = {
-"#F7F7F2":'Baby Poweder',
+"#FCE762":'Maize',
 '#E4E6C3':'Beige',
-'#899878':'Moss Green',
-'#222725':'Eerie black',
-'#121113':'Night'
+'#FFB17A':'Sandy Brown',
+'#4F4789':'Ultra Violet',
+'#201335':'Dark Purpel'
 }
 
 
@@ -13,16 +13,15 @@ const select = document.getElementById("color-picker");
 
 for (const color in Color_Palette){
   select.innerHTML += `<option value=${color} id="color-option">${Color_Palette[color]}</option>`;
-  document.body.style.background = `${color}`;
 }
 
 select.addEventListener("change", changeColor);
 
 function changeColor(){
+
   const selectedColor = document.getElementById("color-picker")
   let colorName = document.getElementById("color-name");
   document.body.style.backgroundColor = selectedColor.value;
   colorName.innerText = selectedColor.options[selectedColor.selectedIndex].text;
 }
-
 
