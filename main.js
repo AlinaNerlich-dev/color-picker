4f7a4d1091733c15c21797353be87e461cbe73b9
Alina <info@alina-nerlich.com>
2023-11-23 13:37:26 +0100

    change for...in into Object.entries

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-import "./style.css";
+import './style.css';
 
-const Color_Palette = {
-"#FCE762":'Maize',
-'#E4E6C3':'Beige',
-'#FFB17A':'Sandy Brown',
-'#4F4789':'Ultra Violet',
-'#201335':'Dark Purpel'
-}
+const ColorPalette = {
+  '#FCE762': 'Maize',
+  '#E4E6C3': 'Beige',
+  '#FFB17A': 'Sandy Brown',
+  '#4F4789': 'Ultra Violet',
+  '#201335': 'Dark Purpel',
+};
 
-const select = document.getElementById("color-picker");
+const select = document.getElementById('color-picker');
 
-for (const color in Color_Palette){
-  select.innerHTML += `<option value=${color} id="color-option">${Color_Palette[color]}</option>`;
-}
+// eslint-disable-next-line no-return-assign
+Object.entries(ColorPalette).forEach((color) => select.innerHTML += `<option value=${color} id="color-option">${ColorPalette[color]}</option>`);
 
-select.addEventListener("change", changeColor);
-
-function changeColor(){
-  const selectedColor = document.getElementById("color-picker");
-  let colorName = document.getElementById("color-name");
+function changeColor() {
+  const selectedColor = document.getElementById('color-picker');
+  const colorName = document.getElementById('color-name');
   document.body.style.backgroundColor = selectedColor.value;
   colorName.innerText = selectedColor.options[selectedColor.selectedIndex].text;
 }
 
+select.addEventListener('change', changeColor);
